587296804f2ddf006dc34ebe7a2ece2a2903ed10
Paschal-Overlord <paschal.edu@outlook.com>
2024-12-15 16:49:47 +0100

    Updated vpc id for the security group

--- main.tf
+++ main.tf
@@ -33,6 +33,7 @@ resource "aws_db_subnet_group" "db_subnet_group" {
 
 resource "aws_security_group" "rds_sg" {
   name = "rds-sg"
+  vpc_id = data.aws_vpc.vpc_dops01
 
   ingress {
     from_port        = 3306
